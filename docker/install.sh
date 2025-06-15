486c51e4a3aadcbbd71958437a20de084c7cc0fb
Denis von Arx <dvarx@ethz.ch>
2025-06-15 21:10:25 +0200

    modified required CPP standard

--- docker/install.sh
+++ docker/install.sh
@@ -11,7 +11,7 @@ update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 apt install -y software-properties-common
 add-apt-repository universe -y
-apt update && apt install curl -y
+apt update && apt install curl -y #
 curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
 #install ros dev tools
