1cceeb1590b9e5de06d803ee92b7acaa92decdce
dvarx <dvarx@ethz.ch>
2025-07-16 11:03:16 +0200

    adjusted localization node

--- em_tracking_py/localization_node.py
+++ em_tracking_py/localization_node.py
@@ -31,7 +31,7 @@ if(databasepath==None):
     exit(1)
 
 ks=[70,81,98]
-last_pos=(4,4,4)
+grid_spacing_m=2e-2
 sensorsigpowercalfile=databasepath+"/em_tracking/measurements/calibrations/sens13_sigpowcal.pcal"
 sensorphasorcalfile=databasepath+"/em_tracking/measurements/calibrations/refsensor_phasorcal.pcal" 
 fieldmapcalfile=databasepath+"/em_tracking/measurements/calibrations/fieldmap_16cm16cm16cm_60Hz_refsensor.pkl"
@@ -43,10 +43,14 @@ with open(fieldmapcalfile,"rb") as fptr:
     data=pkl.load(fptr)
 sigpowerinterp=data["sigpowerinterpolator"]
 phasormatinterp=data["phasormatinterp"]
+Nx=max(sigpowerinterp.grid[0])
+Ny=max(sigpowerinterp.grid[1])
+Nz=max(sigpowerinterp.grid[2])
+last_pos=(Nx/2,Ny/2,Nz/2)
 #define localization method
-def localize(sigpowers,initial_gues=(4,4,4)):
+def localize(sigpowers,initial_guess=(Nx/2,Ny/2,Nz/2)):
     residuals=lambda pos : 1e6*(sigpowerinterp(pos)-sigpowers).flatten()
-    res=least_squares(residuals,(4,4,4),bounds=([0,0,0],[8,8,8]),ftol=1e-5,gtol=1e-5)
+    res=least_squares(residuals,initial_guess,bounds=([0,0,0],[8,8,8]),ftol=1e-5,gtol=1e-5)
     return res
 
 #transform matrix used for aligning catheter to emns frame, use __comp_rot_mat for computation
@@ -54,10 +58,10 @@ trans=np.eye(3)#np.array([[-1,0,0],[0,1,0],[0,0,1]])
 
 #low pass filter related
 low_pass_pos_filter=True
-xs=0
-ys=0
-zs=0
-#xs=alpha*xs+(1-alpha)*x
+xest_filtered=0
+yest_filtered=0
+zest_filtered=0
+#xest_filtered=alpha*xest_filtered+(1-alpha)*x
 alpha=0.0
 
 use_ref_sensor=True
@@ -66,7 +70,7 @@ reference_powers_list=[]
 reference_powers=np.zeros(3)
 refpowers_counter=0
 def voltage_frames_cb(msg):
-    global xs,ys,zs, last_pos, loc_node
+    global xest_filtered,yest_filtered,zest_filtered, last_pos, loc_node
     global meas_counter,corr_factors,refpowers_counter, reference_powers
     #data[sampleno,channelno]
     data=np.array(msg.data).reshape(2000,4)
@@ -84,7 +88,7 @@ def voltage_frames_cb(msg):
         signal_powers[1]=signal_powers[1]*reference_powers[1]/np.abs(refsensorpows[1])
         signal_powers[2]=signal_powers[2]*reference_powers[2]/np.abs(refsensorpows[2])
     #estimate the position
-    res=localize(signal_powers,initial_gues=last_pos)
+    res=localize(signal_powers,initial_guess=last_pos)
     #estimate the orientation
     phasormatref=phasormatinterp(res.x)[0,:,:]
     last_pos=res.x
@@ -101,16 +105,16 @@ def voltage_frames_cb(msg):
     powersmsg.z=signal_powers[2]
     sigpowers_pub.publish(powersmsg)
     if low_pass_pos_filter:
-        xs=alpha*xs+(1-alpha)*res.x[0]
-        ys=alpha*ys+(1-alpha)*res.x[1]
-        zs=alpha*zs+(1-alpha)*res.x[2]
-        pointmsgstamped.point.x=-(xs*0.02-0.08)
-        pointmsgstamped.point.y=-(ys*0.02-0.08)
-        pointmsgstamped.point.z=-(zs*0.02-0.08)
+        xest_filtered=alpha*xest_filtered+(1-alpha)*res.x[0]
+        yest_filtered=alpha*yest_filtered+(1-alpha)*res.x[1]
+        zest_filtered=alpha*zest_filtered+(1-alpha)*res.x[2]
+        pointmsgstamped.point.x=(xest_filtered-Nx/2)*grid_spacing_m
+        pointmsgstamped.point.y=(yest_filtered-Ny/2)*grid_spacing_m
+        pointmsgstamped.point.z=(zest_filtered-Nz/2)*grid_spacing_m
     else:
-        pointmsgstamped.point.x=-(res.x[0]*0.02-0.08)
-        pointmsgstamped.point.y=-(res.x[1]*0.02-0.08)
-        pointmsgstamped.point.z=-(res.x[2]*0.02-0.08)
+        pointmsgstamped.point.x=(res.x[0]-Nx/2)*grid_spacing_m
+        pointmsgstamped.point.y=(res.x[1]-Ny/2)*grid_spacing_m
+        pointmsgstamped.point.z=(res.x[2]-Nz/2)*grid_spacing_m
     #publish the localized point
     pointmsgstamped.header.frame_id = 'emns'
     pointmsg=pointmsgstamped.point
@@ -125,8 +129,8 @@ def voltage_frames_cb(msg):
     tfmsg.transform.rotation.y=quat[1]
     tfmsg.transform.rotation.z=quat[2]
     tfmsg.transform.rotation.w=quat[3]
-    tfmsg.header.frame_id="pickups"
-    tfmsg.child_frame_id = "emns"
+    tfmsg.header.frame_id="emns"
+    tfmsg.child_frame_id = "pickups"
     #tfmsg.header.stamp=loc_node.get_clock().now()
     transform_pub.sendTransform(tfmsg)
 
